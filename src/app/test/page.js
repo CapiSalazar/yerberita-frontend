bc235080654c6ac5988da8201a17284738537732
Capi <salazarcroc@gmail.com>
2025-03-22 19:22:46 -0600

    🚀 Prueba de NEXT_PUBLIC_API_URL en producción

---
+++ src/app/test/page.js
@@ -0,0 +1,10 @@
+import TestAPIUrl from '@/components/TestAPIUrl';
+
+export default function TestPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-xl font-bold mb-4">Prueba de Variable de Entorno</h1>
+      <TestAPIUrl />
+    </div>
+  );
+}
